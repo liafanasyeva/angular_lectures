f8587f9e1ba0e5d9d8358baf6a943edac3d564f8
liafanasyeva <afanasyevaelly1@gmail.com>
2020-03-24 16:43:15 +0300

    Init

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokemonListComponent,
+    TopBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
