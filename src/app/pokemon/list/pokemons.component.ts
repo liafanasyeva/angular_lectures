62d5817e392901efa18c3db6724a3086138694bd
liafanasyeva <afanasyevaelly1@gmail.com>
2020-04-17 01:09:00 +0300

    updated list of pokemons

--- src/app/pokemon/list/pokemons.component.ts
+++ src/app/pokemon/list/pokemons.component.ts
@@ -1,32 +1,36 @@
-import { Component,
-  OnInit,
-   ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  EventEmitter,
-  Output, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Pokemon } from '../../pokemon';
 import { POKEMONS } from '../../../app/database_pokemons';
 
 
 @Component({
-
   selector: 'app-pokemons',
   templateUrl: './pokemons.component.html',
-  styleUrls: ['./pokemons.component.sass'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./pokemons.component.sass']
 })
 
 export class PokemonsComponent implements OnInit {
   pokemons = POKEMONS;
 
-  selectedPokemon: Pokemon;
+  private selectedPokemon: Pokemon;
+  public listOfPokemons: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onSelect(pokemon: Pokemon): void {
+  public onSelect(pokemon: Pokemon): void {
   this.selectedPokemon = pokemon;
+  }
+
+  public changeView(): void {
+    this.listOfPokemons = !this.listOfPokemons;
+    console.log(this.listOfPokemons)
+  }
+
+  public catchPokemon(pokemon: Pokemon): void {
+    pokemon.caught = !pokemon.caught;
+    console.log('Is pokemon ' + pokemon.name +  ' caught? ' + pokemon.caught );
   }
 }
